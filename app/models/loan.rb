bb13a6a0a343fc098a530ae6d40c96d3f1861519
Frankie Roberto <frankie@frankieroberto.com>
2011-07-21 12:05:42 +0100

    Making validation nil-safe for when there is no book associated

--- app/models/loan.rb
+++ app/models/loan.rb
@@ -4,7 +4,7 @@ class Loan < ActiveRecord::Base
   belongs_to  :lender,
               :class_name => "User",
               :foreign_key => :lender_id
-              
+
   belongs_to  :borrower,
               :class_name => "User",
               :foreign_key => :borrower_id
@@ -13,9 +13,9 @@ class Loan < ActiveRecord::Base
 
   validate :borrower_cant_be_lender
   validate :lender_must_own_book
-  
+
   after_create :set_book_status_to_on_loan
-              
+
   def give_back
     if on_loan?
       update_attribute :returned, Time.now
@@ -23,24 +23,24 @@ class Loan < ActiveRecord::Base
     else
       return false # Can't give back the same loan twice
     end
-  end             
-              
+  end
+
   def on_loan?
     returned.nil?
   end
-  
+
   protected
-  
+
   def set_book_status_to_on_loan
     book.update_attribute :status, StaticData::BOOK_STATUS['ONLOAN']
   end
-  
+
   def borrower_cant_be_lender
     errors.add(:borrower, "you can't lend a book to yourself") if borrower == lender
   end
-  
+
   def lender_must_own_book
-    errors.add(:lender, "you must own the book you want to lend") unless book.user == lender
+    errors.add(:lender, "you must own the book you want to lend") unless book && book.user == lender
   end
-  
+
 end
